4c4a654e39f3197b6bb7b41e41fbe5a9d5365e08
willlin1231 <willlin94588@gmail.com>
2025-03-02 22:19:15 +0800

    test for create file

---
+++ test.py
@@ -0,0 +1,7 @@
+file_path = "test.txt"
+
+# 寫入文件
+with open(file_path, "w", encoding="utf-8") as file:
+    file.write("Hello world")
+
+print(f"File '{file_path}' has been created and written successfully.")
